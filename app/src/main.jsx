ac155c0d57ac22b3059cb37409dcb79444576d9b
Saumya Lohani <72060264+SaumyaLohani@users.noreply.github.com>
2026-09-20 21:32:41 +0530

    Create main.jsx

---
+++ app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TriageAssistant from './TriageAssistant.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TriageAssistant />
+  </React.StrictMode>,
+)
